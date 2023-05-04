3520a0729c29f190e50c3579cb059f05ac88a1ad
Marcola <51445096+Marcos-kaneck@users.noreply.github.com>
2023-05-04 19:40:32 -0400

    Update Produto.java

--- udemicurse/src/application/Produto.java
+++ udemicurse/src/application/Produto.java
@@ -1,51 +1,42 @@
-package application;
-
-import entities.Product;
-import java.util.Locale;
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package exercicio2;
+
+
+import entities.Employee;
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  *
  * @author User
  */
-public class Produto {
+public class Exercicio2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Employee emp = new Employee();
         
-        Scanner scan = new Scanner(System.in);
-        
-        Product prod = new Product();
-        
-        System.out.println("Enter product data: ");
         System.out.println("Name: ");
-        prod.name = scan.nextLine();
-        System.out.print("Price: ");
-        prod.price = scan.nextDouble();
-        System.out.print("Quantity in stock: ");
-        prod.quantity = scan.nextInt();
+        emp.name = sc.nextLine();
+        System.out.println("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        emp.tax = sc.nextDouble();
         
-        System.out.println("Product data: "+prod+"\n");
+        System.out.print("Employee: \n"+emp);
         
-        System.out.println("Enteder the number of products to be added in stock: ");
-        int quantity = scan.nextInt();
-        prod.addProducts(quantity);
-        System.out.println("Updated data: "+prod);
+        System.out.println(" Which pertentage to increase salary?");
+        double salary = sc.nextDouble();
+        emp.increaseSalary(salary);
         
-        System.out.println("Enteder the number of products to be remove from stock: ");
-        int quantityRemove = scan.nextInt();
-        prod.removeProducts(quantityRemove);
-        System.out.println("Updated data: "+prod);
-        
-        scan.close();
+        System.out.print("Upadated data: "+emp+"\n");
     }
     
 }
